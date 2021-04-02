99b40fef866faf96886da4b38899a43831df35c9
Ashton Williams <>
2021-04-02 01:36:50 -0400

    Day 1 Project 100 Days of Coding

---
+++ Day 1-10/Day 1/main.js
@@ -0,0 +1,8 @@
+function randomNum() {
+	console.log(Math.floor(Math.random() * 10) + 1);
+	console.log(Math.floor(Math.random() * 100) + 1);
+	console.log(Math.floor(Math.random() * 1000) + 1);
+	console.log(Math.floor(Math.random() * 10000) + 1);
+	console.log(Math.floor(Math.random() * 100000) + 1);
+}
+randomNum();
